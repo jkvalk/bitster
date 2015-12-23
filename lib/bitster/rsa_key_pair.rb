94244431540974f6bd729ea7af441bc954c224b4
J.K.Valk <jkvalk@mail.ru>
2015-12-23 20:55:33 +0200

    Travis, Coveralls

--- lib/bitster/rsa_key_pair.rb
+++ lib/bitster/rsa_key_pair.rb
@@ -27,9 +27,54 @@ module Bitster
 
     end
 
-
     private
     include CryptoMath
+
+    # ToDo: what should the length difference of p and q really be?
+    def gen_p
+      handle_exceptions do
+        len_p = @len/2
+        len_p -= rand(2..4) if @shorter == 0
+        loop do
+          p = gen_odd(len_p)
+          return p if probable_prime?(p)
+        end
+      end
+    end
+
+    # ToDo: what should the length difference of p and q really be?
+    def gen_q
+      handle_exceptions do
+        len_q = @len/2
+        len_q -= rand(2..4) if @shorter == 1
+        loop do
+          q = gen_odd(len_q)
+          return q if probable_prime?(q)
+        end
+      end
+    end
+
+    def gen_totient
+      handle_exceptions do
+        (@p-1)*(@q-1)
+      end
+    end
+
+    # ToDo: what should the lower bound really be?
+    def gen_e
+      handle_exceptions do
+        loop do
+          e = rand(4..@k)
+          return e if e.gcd(@k) == 1
+        end
+      end
+    end
+
+    def gen_d
+      handle_exceptions do
+        modular_multiplicative_inverse(e, k)
+      end
+    end
 
     def handle_exceptions
       begin
@@ -37,42 +82,6 @@ module Bitster
       rescue StandardError => e
         raise RSAKeyPairError, "Exception in RSAKeyPair: #{e}"
       end
-    end
-
-    # ToDo: what should the length difference of p and q really be?
-    def gen_p
-      len_p = @len/2
-      len_p -= rand(2..4) if @shorter == 0
-      loop do
-        p = gen_odd(len_p)
-        return p if probable_prime?(p)
-      end
-    end
-
-    # ToDo: what should the length difference of p and q really be?
-    def gen_q
-      len_q = @len/2
-      len_q -= rand(2..4) if @shorter == 1
-      loop do
-        q = gen_odd(len_q)
-        return q if probable_prime?(q)
-      end
-    end
-
-    def gen_totient
-      (@p-1)*(@q-1)
-    end
-
-    # ToDo: what should the lower bound really be?
-    def gen_e
-      loop do
-        e = rand(4..@k)
-        return e if e.gcd(@k) == 1
-      end
-    end
-
-    def gen_d
-      modular_multiplicative_inverse(e, k)
     end
   end
 end
